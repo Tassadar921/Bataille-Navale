3fa50f971961e10906d926f3aed41417fd4763c0
Tassadar921 <80831213+Tassadar921@users.noreply.github.com>
2022-03-14 17:39:36 +0100

    pieces

--- front/src/app/editor/editor.page.ts
+++ front/src/app/editor/editor.page.ts
@@ -12,10 +12,32 @@ export class EditorPage implements OnInit {
 
   public droppedData = '';
   public matrix = [];
-  public race;
+  public race = 'human';
   public output = '';
 
-  private selected = 'U';
+  private direction = 'U';
+  private model = [
+    {name: 'human1', shape:3},
+    {name: 'human2', shape:4},
+    {name: 'human3', shape:8},
+    {name: 'human4', shape:9},
+    {name: 'enkar1', shape:1},
+    {name: 'enkar2', shape:2},
+    {name: 'enkar3', shape:6},
+    {name: 'enkar4', shape:5},
+    {name: 'arash1', shape:2},
+    {name: 'arash2', shape:3},
+    {name: 'arash3', shape:11},
+    {name: 'arash4', shape:7},
+    {name: 'sunari1', shape:2},
+    {name: 'sunari2', shape:2},
+    {name: 'sunari3', shape:12},
+    {name: 'sunari4', shape:10},
+    {name: 'vyrkul1', shape:2},
+    {name: 'vyrkul2', shape:3},
+    {name: 'vyrkul3', shape:6},
+    {name: 'vyrkul4', shape:7},
+  ];
 
   constructor() {}
 
@@ -28,15 +50,15 @@ export class EditorPage implements OnInit {
         row.push(0);
       }
       this.matrix.push(row);
-      }
+    }
   }
 
-  setSelected = (val) => this.selected=val;
+  setSelected = (val) => this.direction = val;
 
   setId = (key) => {
-    if(this.selected===key){
+    if (this.direction === key) {
       return 'selected';
-    }else{
+    } else {
       return '';
     }
   };
@@ -46,22 +68,70 @@ export class EditorPage implements OnInit {
       ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
       [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
     ];
-    for (let i = 0; i<num[0].length; i++) {
-      if(num[0][i] === letter){
+    for (let i = 0; i < num[0].length; i++) {
+      if (num[0][i] === letter) {
         return num[1][i];
       }
     }
     return -1;
   };
 
-  onDrop = (dropData, letter, num) => {
-    console.log(dropData + ' ' + letter + num);
-    const col = this.letterToNum(letter);
-    if(!this.matrix[num][col]) {
-      console.log(num);
-      console.log(col);
-      this.matrix[num][col] = dropData;
-      console.log(this.matrix);
+  nameToShape = (name) => {
+    for(let i=0;i<this.model.length;i++){
+      if(this.model[i].name===name){
+        return this.model[i].shape;
+      }
+    }
+    return -1;
+  };
+
+  getRotation = (direction) => {
+    switch(direction){
+      case 'U':
+        return '';
+      case 'R':
+        return 'right';
+      case 'D':
+        return 'down';
+      case 'L':
+        return 'left';
+    }
+  };
+
+  onDrop = (dropData, col, line) => {
+    console.log(this.direction);
+    console.log(dropData + ' ' + col + line);
+    col = this.letterToNum(col);
+    switch(this.nameToShape(dropData)){
+      case 1:
+        if(!this.matrix[line][col]) {
+          this.matrix[line][col] = {name: dropData + '_1.1', direction: this.getRotation(this.direction)};
+        }else{
+          this.output='Unable to put this here';
+        }
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      case 6:
+        break;
+      case 7:
+        break;
+      case 8:
+        break;
+      case 9:
+        break;
+      case 10:
+        break;
+      case 11:
+        break;
+      case 12:
+        break;
     }
   };
 }
